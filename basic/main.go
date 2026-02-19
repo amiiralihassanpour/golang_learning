73d53bdc5a4b58058564a318a79a4b2527f96f02
Amirali Hassanpour <a.hassanpour.official@gmail.com>
2026-02-19 17:03:41 +0330

    do some basic learning of golang
    varibales
    print hello world and ...

--- basic/main.go
+++ basic/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+
 func main() {
+	
 	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to Go programming,", "Let's learn Go together.")
+
+	var name string = "Alice"
+	fmt.Println("My name is", name)
+
+	age := 30
+	fmt.Println("I am", age, "years old.")
+	
+	var a, b int = 5, 10
+	fmt.Println("The sum of", a, "and", b, "is", a+b)
+
+	const pi = 3.14
+	fmt.Println("The value of pi is", pi)
+
 }
